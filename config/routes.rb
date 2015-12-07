c8b451778f189e27a276b51794039af9a232ef4c
Tran Xuan Dung <tran.xuan.dung@framgia.com>
2015-12-07 14:02:10 +0700

    Set up Devise authentication for users and admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+  devise_for :admins
+  devise_for :users
+  root 'home#index'
   get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
